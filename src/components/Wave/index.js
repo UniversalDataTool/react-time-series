40d269915452995483e7ea4dcc8af3f7ab96724d
seveibar <seveibar@gmail.com>
2020-10-16 03:51:25 -0400

    add background grid to wave

--- src/components/Wave/index.js
+++ src/components/Wave/index.js
@@ -2,8 +2,38 @@ import React from "react"
 import range from "lodash/range"
 import { styled } from "@material-ui/core/styles"
 import colorAlpha from "color-alpha"
+import useColors from "../../hooks/use-colors"
 
 const Container = styled("div")({})
+
+const mins = 1000 * 60
+const hours = 60 * mins
+const days = 24 * hours
+const weeks = 7 * days
+const months = 30 * days
+const years = 12 * months
+console.log({
+  mins,
+  hours,
+  days,
+  weeks,
+  months,
+  years,
+})
+const timeIntervals = [100, 1000, mins, hours, days, weeks, months, years]
+
+const findReasonableGridDuration = (duration) => {
+  let bestFittingIntervalIndex = 0
+  let bestFittingIntervalScore = -Infinity
+  for (const [i, timeInterval] of Object.entries(timeIntervals)) {
+    const timeIntervalScore = -1 * Math.abs(duration / timeInterval - 20)
+    if (timeIntervalScore > bestFittingIntervalScore) {
+      bestFittingIntervalIndex = i
+      bestFittingIntervalScore = timeIntervalScore
+    }
+  }
+  return timeIntervals[bestFittingIntervalIndex]
+}
 
 export const Wave = ({
   curves,
@@ -13,9 +43,39 @@ export const Wave = ({
   durationGroups = [],
   timestamps = [],
 }) => {
+  const colors = useColors()
+
+  const { x: startTimeOnGraph } = transformMatrix.inverse().applyToPoint(0, 0)
+  const { x: endTimeOnGraph } = transformMatrix.inverse().applyToPoint(500, 0)
+
+  const gridDuration = findReasonableGridDuration(
+    endTimeOnGraph - startTimeOnGraph
+  )
+  const numberOfGridLines = Math.ceil(
+    (endTimeOnGraph - startTimeOnGraph) / gridDuration
+  )
+
   return (
     <Container style={{ curves, width, height }}>
       <svg width={width} height={height}>
+        {range(numberOfGridLines + 1).map((i) => {
+          const { x: lineX } = transformMatrix.applyToPoint(
+            Math.floor(startTimeOnGraph / gridDuration) * gridDuration +
+              gridDuration * i,
+            0
+          )
+
+          return (
+            <line
+              key={i}
+              stroke={colors.Selection}
+              x1={lineX}
+              x2={lineX}
+              y1={0}
+              y2={height}
+            />
+          )
+        })}
         {durationGroups.flatMap(({ durations, color }) => {
           return durations.map((duration) => {
             const { x: startX } = transformMatrix.applyToPoint(
