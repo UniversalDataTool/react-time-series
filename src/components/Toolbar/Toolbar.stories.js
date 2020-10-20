53c556187f86a8c93c1d329d2b5b96b7b74abaac
seveibar <seveibar@gmail.com>
2020-10-20 02:23:24 -0400

    boilerplate toolbar

---
+++ src/components/Toolbar/Toolbar.stories.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Toolbar from "./"
+
+export default {
+  title: "Toolbar",
+  component: Toolbar,
+}
+
+export const Primary = () => {
+  return <Toolbar />
+}
---
+++ src/components/Toolbar/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Toolbar = () => {
+  return <div>asd</div>
+}
+
+export default Toolbar
