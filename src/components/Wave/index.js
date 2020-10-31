f660464ea7b1cdf4d06d371ceb766bfc1da0d3f5
seveibar <seveibar@gmail.com>
2020-10-31 16:33:57 -0400

    refactor out visible point calculation

--- src/components/Wave/index.js
+++ src/components/Wave/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useMemo } from "react"
 import range from "lodash/range"
 import { styled } from "@material-ui/core/styles"
 import colorAlpha from "color-alpha"
@@ -10,8 +10,6 @@ const userSelectOffStyle = {
   whiteSpace: "pre",
   fontVariantNumeric: "tabular-nums",
 }
-
-const Container = styled("div")({})
 
 const reduceForVisibleDuration = (data, startTime, visibleDuration, width) => {
   const firstInnerIndex = data.findIndex(([t]) => t >= startTime)
@@ -79,127 +77,130 @@ export const Wave = ({
     minorGridLinePixelDistance,
   } = gridLineMetrics
 
+  const visibleTransformedPointsOnCurves = useMemo(() => {
+    const visibleTransformedPointsOnCurves = []
+    for (const curve of curves) {
+      visibleTransformedPointsOnCurves.push(
+        reduceForVisibleDuration(
+          curve.data,
+          startTimeOnGraph,
+          visibleDuration,
+          width
+        ).map(([t, y]) => {
+          return transformMatrix.applyToPoint(t, y)
+        })
+      )
+    }
+    return visibleTransformedPointsOnCurves
+  }, [transformMatrix, curves, startTimeOnGraph, visibleDuration, width])
+
   return (
-    <Container style={{ curves, width, height }}>
-      <svg width={width} height={height}>
-        {range(-5, numberOfMajorGridLines + 1).map((i) => {
-          const timeAtLine =
-            Math.floor(startTimeOnGraph / majorDuration) * majorDuration +
-            majorDuration * i
+    <svg width={width} height={height}>
+      {range(-5, numberOfMajorGridLines + 1).map((i) => {
+        const timeAtLine =
+          Math.floor(startTimeOnGraph / majorDuration) * majorDuration +
+          majorDuration * i
 
-          const lineX =
-            majorGridLinePixelOffset + majorGridLinePixelDistance * i
+        const lineX = majorGridLinePixelOffset + majorGridLinePixelDistance * i
 
-          const globalTimelineIndex = Math.floor(timeAtLine / majorDuration)
+        const globalTimelineIndex = Math.floor(timeAtLine / majorDuration)
 
-          let textElm = null
-          if (globalTimelineIndex % 1 === 0) {
-            const timeLines = formatTime(
-              timeAtLine,
-              timeFormat,
-              visibleDuration
-            ).split("\n")
-            textElm = timeLines.map((tl, i) => (
-              <text
-                key={i}
-                x={lineX + 5}
-                y={12 + i * 12}
-                fill={colors.base0}
-                fontSize={12}
-                pointerEvents="none"
-                style={userSelectOffStyle}
-              >
-                {tl}
-              </text>
-            ))
-          }
+        let textElm = null
+        if (globalTimelineIndex % 1 === 0) {
+          const timeLines = formatTime(
+            timeAtLine,
+            timeFormat,
+            visibleDuration
+          ).split("\n")
+          textElm = timeLines.map((tl, i) => (
+            <text
+              key={i}
+              x={lineX + 5}
+              y={12 + i * 12}
+              fill={colors.base0}
+              fontSize={12}
+              pointerEvents="none"
+              style={userSelectOffStyle}
+            >
+              {tl}
+            </text>
+          ))
+        }
 
-          return (
-            <Fragment key={i}>
-              {i >= 0 && (
-                <line
-                  opacity={0.5}
-                  stroke={colors.base01}
-                  x1={lineX}
-                  x2={lineX}
-                  y1={0}
-                  y2={height}
-                />
-              )}
-              {textElm}
-            </Fragment>
-          )
-        })}
-        {numberOfMajorGridLines < 12 &&
-          range(numberOfMinorGridLines).map((i) => {
-            const lineX =
-              minorGridLinePixelOffset + minorGridLinePixelDistance * i
-            return (
+        return (
+          <Fragment key={i}>
+            {i >= 0 && (
               <line
-                key={i}
-                opacity={0.25}
+                opacity={0.5}
                 stroke={colors.base01}
                 x1={lineX}
                 x2={lineX}
                 y1={0}
                 y2={height}
               />
-            )
-          })}
-        {durationGroups.flatMap(({ durations, color: dgColor }, dgi) => {
-          return durations.map((duration, di) => {
-            const { x: startX } = transformMatrix.applyToPoint(
-              duration.start,
-              0
-            )
-            const { x: endX } = transformMatrix.applyToPoint(duration.end, 0)
-            if (isNaN(startX) || isNaN(endX)) return null
-            return (
-              <rect
-                key={`${dgi},${di}`}
-                fill={colorAlpha(duration.color || dgColor, 0.2)}
-                x={startX}
-                y={0}
-                width={endX - startX}
-                height={height}
-              />
-            )
-          })
-        })}
-        {curves.map((curve, i) => (
-          <polyline
-            key={i}
-            stroke={curve.color}
-            fill="transparent"
-            points={reduceForVisibleDuration(
-              curve.data,
-              startTimeOnGraph,
-              visibleDuration,
-              width
-            )
-              .map(([t, y]) => {
-                const p = transformMatrix.applyToPoint(t, y)
-                return `${p.x},${p.y}`
-              })
-              .join(" ")}
-          />
-        ))}
-        {timestamps.map((ts, i) => {
-          const { x } = transformMatrix.applyToPoint(ts.time, 0)
+            )}
+            {textElm}
+          </Fragment>
+        )
+      })}
+      {numberOfMajorGridLines < 12 &&
+        range(numberOfMinorGridLines).map((i) => {
+          const lineX =
+            minorGridLinePixelOffset + minorGridLinePixelDistance * i
           return (
             <line
               key={i}
-              x1={x}
-              x2={x}
+              opacity={0.25}
+              stroke={colors.base01}
+              x1={lineX}
+              x2={lineX}
               y1={0}
               y2={height}
-              stroke={ts.color}
-              strokeWidth={1}
             />
           )
         })}
-      </svg>
-    </Container>
+      {durationGroups.flatMap(({ durations, color: dgColor }, dgi) => {
+        return durations.map((duration, di) => {
+          const { x: startX } = transformMatrix.applyToPoint(duration.start, 0)
+          const { x: endX } = transformMatrix.applyToPoint(duration.end, 0)
+          if (isNaN(startX) || isNaN(endX)) return null
+          return (
+            <rect
+              key={`${dgi},${di}`}
+              fill={colorAlpha(duration.color || dgColor, 0.2)}
+              x={startX}
+              y={0}
+              width={endX - startX}
+              height={height}
+            />
+          )
+        })
+      })}
+      {curves.map((curve, i) => (
+        <polyline
+          key={i}
+          stroke={curve.color}
+          fill="transparent"
+          points={visibleTransformedPointsOnCurves[i]
+            .map((p) => `${p.x},${p.y}`)
+            .join(" ")}
+        />
+      ))}
+      {timestamps.map((ts, i) => {
+        const { x } = transformMatrix.applyToPoint(ts.time, 0)
+        return (
+          <line
+            key={i}
+            x1={x}
+            x2={x}
+            y1={0}
+            y2={height}
+            stroke={ts.color}
+            strokeWidth={1}
+          />
+        )
+      })}
+    </svg>
   )
 }
 
